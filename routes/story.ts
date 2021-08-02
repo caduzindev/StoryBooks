8b83b0b65d7fb1b372d187d7e4c457eded412f75
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2021-08-02 14:51:12 -0300

    Adicionado funcionalidades de storys

--- routes/story.ts
+++ routes/story.ts
@@ -37,39 +37,92 @@ router.get('/',async (req,res)=>{
 })
 
 router.get('/edit/:id',Auth.ensureAuth,async (req,res)=>{
-    const user = <UserInterface>req.user 
-    const story = await Story.findOne({
-        _id:req.params.id
-    }).lean()
-
-    if(!story){
-        return res.render('error/404')
-    }
-    if(story.author != user.id){
-        res.redirect('/stories')
-    }else{
-        res.render('stories/edit',{
-            story
-        })
+    try {
+        const user = <UserInterface>req.user 
+        const story = await Story.findOne({
+            _id:req.params.id
+        }).lean()
+    
+        if(!story){
+            return res.render('error/404')
+        }
+        if(story.author != user.id){
+            res.redirect('/stories')
+        }else{
+            res.render('stories/edit',{
+                story
+            })
+        }
+    } catch (error) {
+        console.error(error)
+        return res.render('error/500')
     }
 })
 
 router.put('/:id',Auth.ensureAuth,async (req,res)=>{
-    const user = <UserInterface>req.user
-    let story = await Story.findById(req.params.id)
-
-    if(!story){
-        res.render('error/404')
-    }
-    if(story.author != user.id){
-        res.redirect('/stories')
-    }else{
-        story = await Story.findOneAndUpdate({_id:req.params.id},req.body,{
-            new:true,
-            runValidators:true
-        })
-        res.redirect('/dashboard')
+    try {
+        const user = <UserInterface>req.user
+        let story = await Story.findById(req.params.id).lean()
+    
+        if(!story){
+            res.render('error/404')
+        }
+        if(story.author != user.id){
+            res.redirect('/stories')
+        }else{
+            story = await Story.findOneAndUpdate({_id:req.params.id},req.body,{
+                new:true,
+                runValidators:true
+            })
+            res.redirect('/dashboard')
+        }
+    } catch (error) {
+        console.error(error)
+        return res.render('error/500')
     }
 })
 
+router.delete('/:id',Auth.ensureAuth,async (req,res)=>{
+    try {
+        await Story.remove({_id:req.params.id})
+        res.redirect('/dashboard')
+    } catch (error) {
+        console.error(error)
+        return res.render('error/500')
+    }
+})
+
+router.get('/:id',Auth.ensureAuth,async (req,res)=>{
+   try {
+       let story = await Story.findById(req.params.id).lean()
+        .populate('author')
+        .lean()
+
+        if(!story){
+            return res.render('error/404')
+        }
+        res.render('stories/show',{
+            story
+        })
+   } catch (error) {
+       console.error(error)
+       res.render('error/500')
+   }
+})
+
+router.get('/user/:userId',Auth.ensureAuth,async (req,res)=>{
+    try {
+        const stories = await Story.find({
+            author:req.params.userId,
+            status:'public'
+        }).populate('author').lean()
+        
+        res.render('stories/index',{
+            stories
+        })
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
+    }
+})
 export default router;
