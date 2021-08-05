d3f38b54a1c9be01e7d6f1f9c12e158e2dff6b23
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2021-08-05 12:48:05 -0300

    Separando responsabilidades da aplicação, criando camada de controller

---
+++ app/controllers/Story/StoryRenderAdd.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import { Controller } from "../contracts/controller";
+
+export class StoryRenderAdd implements Controller<void>{
+    constructor(){}
+    public handle(req:Request,res:Response){
+        res.render('add')
+    }
+}
